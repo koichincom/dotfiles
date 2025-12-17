6f3cd103c9052b4b72b27bf91da5609ec9347f91
Koichi Nakayamada <151496024+koichincom@users.noreply.github.com>
2025-12-16 18:08:03 -0800

    Refactor LSP config into dedicated module

---
+++ nvim/.config/nvim/lua/modules/lsp.lua
@@ -0,0 +1,43 @@
+-- LSP feature module using Neovim 0.11+ native APIs
+local M = {}
+
+function M.init()
+    -- Server-specific settings
+    vim.lsp.config("lua_ls", {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { "vim" },
+                },
+            },
+        },
+    })
+
+    -- Enable LSP servers
+    vim.lsp.enable({
+        "lua_ls",
+        "pyright",
+        "ts_ls",
+        "clangd",
+        "html",
+        "cssls",
+        "jsonls",
+        "astro",
+        "glsl_analyzer",
+        "zls",
+    })
+
+    -- LSP navigation keymaps (Telescope integration)
+    local telescope_builtin = require("telescope.builtin")
+
+    -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+    -- vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, { desc = "Go to definition" })
+    -- vim.keymap.set("n", "gt", telescope_builtin.lsp_type_definitions, { desc = "Go to type definition" })
+    -- vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, { desc = "Go to implementation" })
+    -- vim.keymap.set("n", "gr", telescope_builtin.lsp_references, { desc = "Find references" })
+    -- vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+    -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature help" })
+    -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+end
+
+return M
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,4 +1,4 @@
--- LSP configuration
+-- nvim-lspconfig: Provides baseline LSP server configurations
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -6,30 +6,6 @@ return {
         "mason-lspconfig.nvim",
     },
     config = function()
-        -- Avoid "Undefined global 'vim'" diagnostics for Lua files
-        vim.lsp.config("lua_ls", {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
-
-        vim.lsp.enable {
-            "lua_ls",
-            "pyright",
-            "ts_ls",
-            "clangd",
-            "html",
-            "cssls",
-            "jsonls",
-            "astro",
-            "glsl_analyzer",
-        }
-
-        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+        require("modules.lsp").init()
     end,
 }
