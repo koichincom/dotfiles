4996a8a5fd38d7f28c4762212f0a28984c24b1e9
Koichi Nakayamada <151496024+koichincom@users.noreply.github.com>
2025-12-16 18:08:35 -0800

    Configure colorizer.nvim with detailed options

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,1 +1,26 @@
-return { "catgoose/nvim-colorizer.lua" }
+return {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    config = function()
+        require("colorizer").setup({
+            filetypes = { "*" },
+            user_default_options = {
+                RGB = true,
+                RRGGBB = true,
+                names = false,
+                RRGGBBAA = true,
+                AARRGGBB = true,
+                rgb_fn = true,
+                hsl_fn = true,
+                css = true,
+                css_fn = true,
+                mode = "background",
+                tailwind = true,
+                sass = { enable = true, parsers = { "css" } },
+                virtualtext = "■",
+                always_update = false,
+            },
+            buftypes = {},
+        })
+    end,
+}
