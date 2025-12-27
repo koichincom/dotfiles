83a28651c374c07f99b929c77a0707c1cbaa9a58
Koichi Nakayamada <koichi.nakayamada@gmail.com>
2025-12-27 09:48:40 -0800

    fix: Prevent treesitter parsers from reinstalling on startup
    Move nvim-treesitter install() call from config to build function.
    Previously parsers were re-downloaded/compiled on every Neovim start.
    
    Update lazy-lock.json with plugin version changes.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,43 @@
+-- nvim-treesitter: Parser management and syntax queries
+-- API updated for nvim-treesitter main branch (Dec 2025)
+-- See: https://github.com/nvim-treesitter/nvim-treesitter
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate", -- Automatically update parsers on plugin update
+    lazy = false,
+    build = function()
+        -- Install parsers only during plugin build/update, not on every startup
+        require("nvim-treesitter").install({
+            "astro",
+            "bash",
+            "c",
+            "cpp",
+            "csv",
+            "css",
+            "glsl",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "r",
+            "sql",
+            "toml",
+            "tsx",
+            "typescript",
+            "tsv",
+            "xml",
+            "yaml",
+            "zig",
+        })
+    end,
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "astro",
-                "bash",
-                "c",
-                "cpp",
-                "csv",
-                "css",
-                "glsl",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "python",
-                "sql",
-                "toml",
-                "tsx",
-                "typescript",
-                "tsv",
-                "yaml",
-                "toml",
-                "xml",
-                "r",
-                "markdown_inline",
-                "zig",
-            },
-            highlight = {
-                enable = true,
-            },
+        -- Enable treesitter-based syntax highlighting for all supported filetypes
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
         })
     end,
 }
